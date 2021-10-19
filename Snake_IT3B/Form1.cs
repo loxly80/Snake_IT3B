d201ee05eff15c40b8e89ec42792871f76d3ce75
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-10-19 13:06:22 +0200

    generování prvků formuláře

--- Snake_IT3B/Form1.cs
+++ Snake_IT3B/Form1.cs
@@ -12,10 +12,28 @@ namespace Snake_IT3B
 {
     public partial class Form1 : Form
     {
+        PictureBox had1;
+        PictureBox had2;
+
         public Form1()
         {
             InitializeComponent();
-            var i = 1;
+            VytvorHady();
+        }
+
+        private void VytvorHady()
+        {
+            had1 = new PictureBox();
+            had1.Size = new Size(50, 50);
+            had1.Location = new Point(10, 10);
+            had1.BackColor = Color.Blue;
+            this.Controls.Add(had1);
+
+            had2 = new PictureBox();
+            had2.Size = new Size(50, 50);
+            had2.Location = new Point(ClientSize.Width-60, 10);          
+            had2.BackColor = Color.Red;
+            this.Controls.Add(had2);
         }
 
     }
